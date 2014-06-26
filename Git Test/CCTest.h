0a6f138ab76a52f65ff80414147a6c733ab59edb
Phil <mrphil1@sky.com>
2014-06-26 18:50:07 +0100

    added test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Phil Evans on 26/06/2014.
+//  Copyright (c) 2014 Phil Evans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
